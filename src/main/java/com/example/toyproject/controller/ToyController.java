d791d4c7d3bd570fea74d0db149ccc29ba69bba1
kimyoungjae <newy12@naver.com>
2022-05-22 20:31:43 +0900

    젠킨스테스트2

--- src/main/java/com/example/toyproject/controller/ToyController.java
+++ src/main/java/com/example/toyproject/controller/ToyController.java
@@ -58,7 +58,7 @@ public class ToyController {
                 model.addAttribute("item", itemService.findByCategory(ItemType.Acc));
                 return "topClothes";
         }
-        //젠킨스 테스트2
+        //젠킨스 테스트23
         return null;
     }
 
